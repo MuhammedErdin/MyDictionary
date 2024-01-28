b3b1927509af0442da8a1d831f1f3827da90ed27
MuhammedErdin <erdinmuhammed835@gmail.com>
2024-01-28 20:56:53 +0300

    Add project files.

---
+++ MyDictionary/Dictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Dictionary<Key, Value>
+    {
+        Key[] keys;
+        Value[] values;
+        public Dictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+
+        public void Add(Key id, Value name)
+        {
+            Key[] tempkey = keys;
+            Value[] tempvalue = values;
+
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+
+            for (int i = 0; i < tempkey.Length; i++)
+            {
+                keys[i] = tempkey[i];
+                values[i] = tempvalue[i];
+            }
+            keys[keys.Length - 1] = id;
+            values[values.Length - 1] = name;
+        }
+
+        public int KLenght
+        {
+            get { return keys.Length; }
+        }
+
+        public Key[] TitleK
+        {
+            get { return keys; }
+        }
+
+        public Value[] TitleV
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>() { };
+            dictionary.Add(1, "Engin");
+            dictionary.Add(2, "Kerem");
+            dictionary.Add(3, "Halil");
+
+            for (int i = 0; i < dictionary.KLenght; i++)
+            {
+                Console.WriteLine(dictionary.TitleK[i] + " numaralı kişi: " + dictionary.TitleV[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
